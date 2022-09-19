3f015efe64df19792b701a01bb5ee0d6d5661719
RitiKSiBJr <letter.ritik060@gmail.com>
2022-09-19 10:17:22 +0545

    3

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup (
+    name = "Scrape imdb and leetcode",
+    version = "1.0.0",
+    description="imdb",
+    author = "RitiK",
+    url = "https://github.com/RitikSibjr/imdb-and-leetcode",
+    packages= ["leet","main"]
+)
