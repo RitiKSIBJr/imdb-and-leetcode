f6fe8804f739a9bb7020943380fc5237beb7a3ef
RitiKSiBJr <letter.ritik060@gmail.com>
2022-09-20 14:54:07 +0545

    changed

--- scrape/leet.py
+++ scrape/leet.py
@@ -1,17 +1,19 @@
+from dataclasses import field
 from bs4 import BeautifulSoup
 import requests
 from requests.exceptions import HTTPError
 import csv
+import os
 
 def check_url(url):
     try:
         res = requests.get(url)
         res.raise_for_status
     except HTTPError as http_err:
-        print(http_err)  
+        return http_err 
         
     except Exception as e:
-        print(e)
+        return e
 
     else:
         return res
@@ -19,20 +21,35 @@ def check_url(url):
 def main():
     res = check_url("https://leetcode.com/problemset/all/")
 
-    soup = BeautifulSoup(res.text,"html.parser")
+    file_size = os.path.getsize("file2.txt")
 
-    problems = soup.find_all("div", class_ = "odd:bg-layer-1 even:bg-overlay-1 dark:odd:bg-dark-layer-bg dark:even:bg-dark-fill-4")
+    if str(res) == "<Response [200]>":
 
-    for problem in problems:
-        sn = problem.find("div", class_="truncate overflow-hidden").text.split(".")[0]
-        ques =  problem.find("div", class_="truncate overflow-hidden").text.split(".")[1]
-        per =  problem.find("span").text
-        # diff = problem.find("div", class_="mx-2 py-[11px]").span.text
-        
-        with open ("file2.txt","a") as file:
-            writer = csv.DictWriter(file,fieldnames=["S.N", "Question", "Acceptance"])
+        if file_size == 0:
+            soup = BeautifulSoup(res.text,"html.parser")
 
-            writer.writerow({"S.N":sn, "Question":ques, "Acceptance":per})  
+            problems = soup.find_all("div", class_ = "odd:bg-layer-1 even:bg-overlay-1 dark:odd:bg-dark-layer-bg dark:even:bg-dark-fill-4")
+
+            for problem in problems:
+                sn = problem.find("div", class_="truncate overflow-hidden").text.split(".")[0]
+                ques =  problem.find("div", class_="truncate overflow-hidden").text.split(".")[1]
+                per =  problem.find("span").text
+                # diff = problem.find("div", class_="mx-2 py-[11px]").span.text
+                
+                with open ("file2.txt","a") as file:
+                    writer = csv.DictWriter(file,fieldnames=["S.N", "Question", "Acceptance"])
+
+                    writer.writerow({"S.N":sn, "Question":ques, "Acceptance":per})  
+
+        else:
+            with open("file2.txt") as file:
+                reader = csv.DictReader(file,fieldnames=["S.N", "Question", "Acceptance"])
+                for line in reader:
+                    print(line["S.N"] + " ", line["Question"] + " ", line["Acceptance"])
+
+    else:
+        print(res)
+
 
 if __name__ == "__main__":
     main()
